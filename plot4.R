abe04475d9f41ce1566d16dc2028d83fe8a7eb31
Ted Yun <ted.yun@oracle.com>
2015-02-04 00:43:06 -0500

    removed legend border for plot 4

--- plot4.R
+++ plot4.R
@@ -19,6 +19,7 @@ plotdata$Time <- strptime(paste(plotdata$Date, plotdata$Time), "%Y-%m-%d %H:%M:%
 # open a png file
 png("plot4.png", width=480, height=480)
 
+# set 2x2 grid
 par(mfrow = c(2, 2))
 
 ## plot first graph
@@ -37,7 +38,7 @@ lines(plotdata$Time, plotdata$Sub_metering_2, col="red")
 lines(plotdata$Time, plotdata$Sub_metering_3, col="blue")
 
 # add legend
-legend("topright", lty = c(1, 1, 1), lwd = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", bty="n", lty = c(1, 1, 1), lwd = c(1, 1, 1), col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## plot fourth graph
 plot(plotdata$Time, plotdata$Global_reactive_power, ylab = "Global_reactive_power", xlab= "datetime", type="l")
